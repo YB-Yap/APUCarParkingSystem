498889639fb02b61e40785338724e99b3c971865
Yap Bo Yang <boyangyap@gmail.com>
2020-10-30 17:00:24 +0800

    Added helpers

---
+++ app/helpers.php
@@ -0,0 +1,28 @@
+<?php
+
+if (!function_exists('getSubsAvailability')) {
+    function getSubsAvailability()
+    {
+        // get available subscription
+        // return count
+
+    }
+}
+
+
+if (!function_exists('getParkingAvailability')) {
+    function getParkingAvailability()
+    {
+        // get available parking spaces
+        // return array
+
+    }
+}
+
+
+if (!function_exists('estimateSubsRestockDate')) {
+    function estimateSubsRestockDate()
+    {
+
+    }
+}
